7f1e3142280e32f15cbfadd6dc62b6a7f58028ef
nephros <nemo@pgxperiiia10>
2024-09-18 21:52:06 +0200

    Add zypper-based pkgconfig script

---
+++ tools/pkgconfig-search-repo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+repos="jolla"
+if [ ! -z "$1" ]; then
+repos="$@"
+else
+	printf "I: no repo names given on command line, using default: '%s'\n" "$repos" >/dev/stderr
+fi
+for repo in $repos
+do
+	printf "I: Packages from repo %s\n" "$repo" >/dev/stderr
+	if [ $EUID = 0 ]; then
+		printf "I: Refreshing repo %s\n" "$repo" >/dev/stderr
+		zypper ref -r $repo >/dev/null
+	else
+	printf "W: Not being run as root, can not refresh repo.\n" > /dev/stderr
+	fi
+	for p in $(zypper search -r $repo devel | awk 'FS="|" {print $2}'|grep devel|sort -u)
+	do
+		info=$(zypper info --provides "$p" | grep pkgconfig | cut -d"=" -f1 | sed 's/[[:space:]]*//g' )
+		if [ -z "$info" ]; then
+			printf "I: no pkgconfig file in: %s\n" "$p" >/dev/stderr
+		else
+			printf "%s,%s\n" "$p" "$info"
+		fi
+	done
+done
