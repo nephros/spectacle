4f9fa55f01bd6e70a2895f49c6a60b3f79a2d08b
Peter G. (nephros) <sailfish@nephros.org>
2025-02-17 08:29:17 +0100

    Use more verbose zypper parameters

--- tools/pkgconfig-search-repo.sh
+++ tools/pkgconfig-search-repo.sh
@@ -11,13 +11,13 @@ do
 	printf "I: Packages from repo %s\n" "$repo" >/dev/stderr
 	if [ $EUID = 0 ]; then
 		printf "I: Refreshing repo %s\n" "$repo" >/dev/stderr
-		zypper ref -r $repo >/dev/null
+		zypper --quiet refresh --repo $repo >/dev/null
 	else
 	printf "W: Not being run as root, can not refresh repo.\n" > /dev/stderr
 	fi
-	for p in $(zypper search -r $repo devel | awk 'FS="|" {print $2}'|grep devel|sort -u)
+	for p in $(zypper --terse --quiet --no-refresh search --repo $repo devel 2>/dev/null | awk 'FS="|" {print $2}'|grep devel|sort -u)
 	do
-		info=$(zypper info --provides "$p" | grep pkgconfig | sed "s/[[:space:]]*//g;s/=.*$//;s/\\(.*\\)/$p,\\1/")
+		info=$(zypper --terse --quiet --no-refresh info --provides "$p" | grep pkgconfig | sed "s/[[:space:]]*//g;s/=.*$//;s/\\(.*\\)/$p,\\1/")
 		if [ -z "$info" ]; then
 			printf "I: no pkgconfig file in: %s\n" "$p" >/dev/stderr
 		else
